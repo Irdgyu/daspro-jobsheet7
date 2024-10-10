d403f4112bbff34ad02d513cfef9fa7c8c6bfbdf
Irdgyu <gilangbayuirwana@gmail.com>
2024-10-10 13:24:59 +0700

    Lulus apa engga ya?

--- SiakadFor06.java
+++ SiakadFor06.java
@@ -4,6 +4,7 @@ public class SiakadFor06 {
     public static void main(String[] args) {
         Scanner input6 = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int tidakLulus = 0, lulus = 0;
 
         for (int i = 1; i <= 10; i++ ) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,8 +15,16 @@ public class SiakadFor06 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai < 60) {
+                tidakLulus +=1;
+            }
+            else{
+                lulus +=1;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
     }
 }
